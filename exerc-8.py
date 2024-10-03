6fb4e6e7877e2eb96aa6dcce7569e3c38cc59f63
Coutinho <rfccrj0@gmail.com>
2024-10-03 20:15:22 -0300

    Create exerc-8.py

---
+++ exerc-8.py
@@ -0,0 +1,10 @@
+input ("Qual a quantidade de votos brancos?")
+
+input ("qual a quantidade de votos nulos?")
+
+input ("qual a quantidade de votos validos?")
+
+print ("total de porcentagens")
+print ("total de votos validos: 100 ")
+print ("total de votos em branco:200")
+print ("total de votos nulos:50 ")
